7c8ae2a93ef805cbd33f82a3ae85fa39062a70e2
Maxim Avanov <maxim.avanov@gmail.com>
2014-03-29 02:25:58 +0400

    Code refactoring

--- reef/views/__init__.py
+++ reef/views/__init__.py
@@ -8,6 +8,9 @@ def index(request):
 
 @view_config(route_name='reef.home', renderer='/reef/index.plim')
 def home(request):
-    users_repo = request.db.repositories.get('users')
+    """
+    :type request: :class:`pyramid.request.Request`
+    """
+    users_repo = request.db.get_repository('users')
     user = users_repo.get_by_id(1)
     return {'project': 'Pacific', 'user': user}
